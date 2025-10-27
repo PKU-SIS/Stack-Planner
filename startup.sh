ab9293244b2bc85cc215b22f2fac1b2d41cf7d0f
nostradamus <86726217@qq.com>
2025-10-27 23:01:03 +0800

    更新启动配置，将端口从8082更改为8555，并调整大纲消息的生成顺序以优化输出格式。

---
+++ startup.sh
@@ -0,0 +1,6 @@
+# export http_proxy=http://localhost:8888
+# export https_proxy=http://localhost:8888
+# export HTTP_PROXY=http://localhost:8888
+# export HTTPS_PROXY=http://localhost:8888
+
+python server.py --port 8555 --log-level debug
