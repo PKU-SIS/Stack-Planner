3dc89e817a2e6776c855d7806a090f296e6607eb
nostradamus <86726217@qq.com>
2025-12-29 16:19:50 +0800

    update ref prompt

--- startup.sh
+++
@@ -1,6 +0,0 @@
-# export http_proxy=http://localhost:8888
-# export https_proxy=http://localhost:8888
-# export HTTP_PROXY=http://localhost:8888
-# export HTTPS_PROXY=http://localhost:8888
-
-python server.py --port 8555 --log-level debug
