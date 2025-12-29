397c4518678c6781838e8afd1549c13a8f3edec6
nostradamus <86726217@qq.com>
2025-12-29 15:04:37 +0800

    update prompt

--- test_llm.py
+++ test_llm.py
@@ -1,15 +1,18 @@
 
 from openai import OpenAI
 client = OpenAI(
-    base_url="http://123.57.228.132:8285",
-    api_key="sk-d47ad54165ee456093bc9ffd599e354e",
-)
+  api_key="1", 
+  base_url="http://10.1.1.212:8000/v1")
 
 response = client.chat.completions.create(
-    model="Qwen2.5-32B-Instruct",
-    messages=[{"role": "user", "content": "Test"}],
-    max_tokens=10,
-    temperature=0.0,
+  model="Qwen3-32B",
+  messages=[{"role": "user", "content": "Why is the sky blue?"}],
+  stream=True,
 )
 
-print(f"📝 Response: {response.choices[0].message.content}")
+# 流式输出响应
+for chunk in response:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end='', flush=True)
+
+print('\n'*2)  # 输出结束后换行
