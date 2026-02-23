4829904acf56d7a3e31a8fd7417d755f08f2f55e
YZZBBB <Teacher3706@sina.com>
2026-02-23 16:27:14 +0800

    重新提交

--- evaluation/Reference/eval_reference_fact.py
+++ evaluation/Reference/eval_reference_fact.py
@@ -9,8 +9,8 @@ import ast
 
 
 def call_model(input):
-    api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkwZDBjNmU0LTI1MzUtNGQ3OS1hOGI4LWUyMGJmYzIwMmIwYSJ9.xCJO76Cj2OMoEo1du9NTj0BI_wZIfYezCk3zbiijjqM"
-    client = OpenAI(api_key=api_key, base_url="http://162.105.88.35:3000/api")
+    api_key = "api"
+    client = OpenAI(api_key=api_key, base_url="url")
 
     response = client.chat.completions.create(
         model="deepseek-v3.2-20251201-160k-local",
